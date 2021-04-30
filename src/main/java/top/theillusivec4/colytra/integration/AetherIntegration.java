3b3a543d71f35f958a6d9ce4c6338500a5ec9b34
bconlon <bconlon@gildedgames.com>
2021-04-29 17:38:50 -0700

    Elytra cape skin can be disabled if the cape render is disabled.

--- src/main/java/top/theillusivec4/colytra/integration/AetherIntegration.java
+++ src/main/java/top/theillusivec4/colytra/integration/AetherIntegration.java
@@ -7,9 +7,12 @@ import top.theillusivec4.curios.api.CuriosApi;
 public class AetherIntegration {
 
   public static void checkCape(RenderElytraEvent evt) {
-    CuriosApi.getCuriosHelper()
-        .findEquippedCurio(stack -> stack.getItem() instanceof CapeItem, evt.getPlayer()).ifPresent(
-        triple -> evt
-            .setResourceLocation(((CapeItem) triple.getRight().getItem()).getCapeTexture()));
+    CuriosApi.getCuriosHelper().findEquippedCurio((item) -> item.getItem() instanceof CapeItem, evt.getPlayer()).ifPresent(triple ->
+            CuriosApi.getCuriosHelper().getCuriosHandler(evt.getPlayer()).ifPresent(handler -> handler.getStacksHandler(triple.getLeft()).ifPresent(stacksHandler -> {
+                CapeItem cape = (CapeItem) triple.getRight().getItem();
+                if (cape.getCapeTexture() != null && stacksHandler.getRenders().get(triple.getMiddle())) {
+                    evt.setResourceLocation(cape.getCapeTexture());
+                }
+            })));
   }
 }
